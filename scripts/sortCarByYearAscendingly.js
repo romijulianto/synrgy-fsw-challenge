083d65cd3befdde9452a1805848a0f0bfc84c538
Romi Julianto <romyjulians@gmail.com>
2023-10-27 20:07:43 +0700

    feat: solve sortCarByYearAscendingly

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -7,6 +7,17 @@ function sortCarByYearAscendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  for (let r = 0; r < result.length; r++) {
+    for (let j = 0; j < result.length - 1; j++) {
+      if (result[j].year > result[j + 1].year) {
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log(result);
 
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
